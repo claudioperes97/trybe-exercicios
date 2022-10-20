4d86559cc5e39b315c3ce5f5d2531378bd91c712
Claudio <contato.cladio@outlook.com>
2022-10-20 15:17:37 -0300

    exercicio 4

---
+++ fundamentos/secao-4-intro-javaScript/dia-1-JavaScript-primeirosPassos/exercicio4.js
@@ -0,0 +1,14 @@
+const correntHour = 23;
+let message = ''
+if (correntHour > 22) {
+    message = 'Não deveríamos comer nada, é hora de dormir'
+} else if (correntHour >= 18 && correntHour < 22) {
+    message = 'Rango da noite, vamos jantar :D'
+} else if (correntHour >= 14 && correntHour < 18) {
+    message = 'Vamos fazer um bolo pro café da tarde?'
+} else if (correntHour >= 11 && correntHour <14) {
+    message = 'Hora do almoço!!!'
+} else {
+    message = 'Hmmm, cheiro de café recém-passado'
+}
+console.log (message);
