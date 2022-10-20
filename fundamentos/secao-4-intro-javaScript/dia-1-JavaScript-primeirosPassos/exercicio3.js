6c6d3b310cc85d669595bcff27f73e69c7d0b3f9
Claudio <contato.cladio@outlook.com>
2022-10-20 15:06:52 -0300

    exercicio 3

---
+++ fundamentos/secao-4-intro-javaScript/dia-1-JavaScript-primeirosPassos/exercicio3.js
@@ -0,0 +1,8 @@
+let nota = 55;
+ if (nota >= 80) {
+    console.log ('aprovada');
+ } else if ( nota < 80 && nota >=60) {
+    console.log ('em espera');
+ } else {
+    console.log ('reprovada');
+ }
