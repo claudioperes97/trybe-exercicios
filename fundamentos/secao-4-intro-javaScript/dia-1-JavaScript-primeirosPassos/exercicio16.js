16729b5817d90774bf29a4942447b70e07ac1453
Claudio <contato.cladio@outlook.com>
2022-10-20 19:17:41 -0300

    exercicio 16

---
+++ fundamentos/secao-4-intro-javaScript/dia-1-JavaScript-primeirosPassos/exercicio16.js
@@ -0,0 +1,6 @@
+const custo = 100
+const imposto = 0.2
+const custoTotal = custo + (custo * imposto);
+const valorDaVenda = 500;
+const lucro = valorDaVenda - custoTotal
+console.log (lucro * 1000);
