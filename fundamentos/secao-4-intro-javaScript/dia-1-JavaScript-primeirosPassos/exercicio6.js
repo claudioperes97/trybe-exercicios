19cee5639fbbfac8686f2d1ac871a5b89182eaea
Claudio <contato.cladio@outlook.com>
2022-10-20 15:44:49 -0300

    exercicio 6

---
+++ fundamentos/secao-4-intro-javaScript/dia-1-JavaScript-primeirosPassos/exercicio6.js
@@ -0,0 +1,14 @@
+let resultado = 'Aprovada';
+switch (resultado) {
+    case 'Aprovada':
+        console.log ('parabens');
+        break;
+    case 'Lista':
+        console.log ('boa sorte');
+        break;
+    case 'Reprovada':
+        console.log ('sinto muito');
+        break
+    default:
+        console.log('opção invalida');
+}
