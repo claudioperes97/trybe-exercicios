81b0a2c8fcc74fdc2a4438d81f8a43ac97c2e394
Claudio <contato.cladio@outlook.com>
2022-10-20 18:25:48 -0300

    exercicio 10

---
+++ fundamentos/secao-4-intro-javaScript/dia-1-JavaScript-primeirosPassos/exercicio10.js
@@ -0,0 +1,8 @@
+let number = -5
+if (number > 0) {
+    console.log ('este numero é positivo')
+}else if (number == 0) {
+    console.log('este numero é zero')
+} else {
+    console.log ('este numero é negativo')
+}
