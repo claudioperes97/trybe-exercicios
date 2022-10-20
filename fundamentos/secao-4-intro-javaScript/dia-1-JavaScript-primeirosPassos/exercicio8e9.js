c9bfbacfd506ed1053973c7981c01a5d65c15f34
Claudio <contato.cladio@outlook.com>
2022-10-20 18:23:14 -0300

    exercicio 8 e 9

---
+++ fundamentos/secao-4-intro-javaScript/dia-1-JavaScript-primeirosPassos/exercicio8e9.js
@@ -0,0 +1,10 @@
+const a = 94
+const b = 6222
+const c = 711
+if (a > b && a > c) {
+    console.log(a + ' é o maior númerio' )
+} else if (b > c && b > a) {
+    console.log (b + ' é o maior número')
+} else {
+    console.log (c + ' é o maior núemro')
+}
