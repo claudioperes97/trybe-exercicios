9b14ec5e1c12285859819840b2afeece4c690cf5
Claudio <contato.cladio@outlook.com>
2022-10-20 18:14:43 -0300

    exercicio 7

---
+++ fundamentos/secao-4-intro-javaScript/dia-1-JavaScript-primeirosPassos/exercicio7.js
@@ -0,0 +1,8 @@
+const a = 5
+const b = 7
+let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b
+console.log(adicao, subtracao, multiplicacao, divisao, modulo);
