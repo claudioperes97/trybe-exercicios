556f603041a11fe7787c6a7de66ad0105a1093bf
Claudio <contato.cladio@outlook.com>
2022-10-20 18:31:44 -0300

    exercicio 11

---
+++ fundamentos/secao-4-intro-javaScript/dia-1-JavaScript-primeirosPassos/exercicio11.js
@@ -0,0 +1,10 @@
+const a = 60
+const b = 60
+const c = 60
+if (a < 0 || b < 0 || c < 0) {
+    console.log ('angulo inválido')
+}else if ((a + b + c) == 180 ) {
+    console.log ('é um triangulo')
+}else {
+    console.log ('não é um triangulo')
+}
